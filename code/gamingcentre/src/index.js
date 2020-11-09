8a564198517c1f496467ac542b6a07727467d05e
SimonFKings <sfk7@student.le.ac.uk>
2020-11-09 02:28:45 +0000

    Added auth0 login/register

--- code/gamingcentre/src/index.js
+++ code/gamingcentre/src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app";
-import { BrowserRouter as Router } from "react-router-dom";
-
+import { BrowserRouter } from "react-router-dom";
+import Auth0ProviderWithHistory from "./auth/auth0-provider-with-history";
 import "./index.css";
 
+
+    
 ReactDOM.render(
-  <Router>
+<BrowserRouter>
+<Auth0ProviderWithHistory>
     <App />
-  </Router>,
+    </Auth0ProviderWithHistory>
+  </BrowserRouter>,
   document.getElementById("root")
 );
