1fca0b1c821f09e69dccfa197da0e48d5e887453
SimonFKings <sfk7@student.le.ac.uk>
2021-02-14 03:09:52 +0000

    Completed edit profile

--- code/gamingcentre/src/views/editProfile.js
+++ code/gamingcentre/src/views/editProfile.js
@@ -1,26 +1,65 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
 import { Loading } from "../components";
 // import { Button } from 'react-native';
 
-
 const EditProfile = () => {
+
   
   const { user } = useAuth0();
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [username, setUsername] = useState("");
-  const [emailAdress, setEmail] = useState("");
+  const [emailAddress, setEmail] = useState("");
 
 
   const {name, picture, email, given_name, family_name, nickname, sub} = user;
+
+
+  useEffect(() => {
+    const getUserMetadata = async () => {
+      const domain = "YOUR_DOMAIN";
+
+      try {
+        const token = await getAccessTokenSilently();
+
+        const response = await fetch(
+          // proxyurl+
+           `https://dev-22x3u4l0.us.auth0.com/api/v2/users/google-oauth2|107476463422329654527`,
+          
+          {    
+            // mode: "no-cors",
+  
+            headers: {
+              Authorization: `Bearer ${token}`,
+              // 'Access-Control-Allow-Origin' : '*',
+  
+              
+            },
+          }
+          );
+
+          const responseData = await response.json();
+          setFirstName(responseData.user_metadata.firstName)
+          setLastName(responseData.user_metadata.lastName)
+          setUsername(responseData.user_metadata.username)
+          setEmail(responseData.user_metadata.emailAddress)
+
+          
+      } catch (e) {
+        console.log(e.message);
+      }
+    };
+    getUserMetadata();
+  }, []);
+
+
   const proxyurl = "https://cors-anywhere.herokuapp.com/";
 
   const { getAccessTokenSilently } = useAuth0();
 
-
-  const callSecureApi = async () => {
+  const updateUser = async () => {
     try {
       const token = await getAccessTokenSilently();
 
@@ -42,23 +81,24 @@ const EditProfile = () => {
             // "origin" : "http://localhost:4040"
 
           },
-          body: JSON.stringify( { "user_metadata": {"family_name" : lastName}})
-          
+          body: JSON.stringify( { "user_metadata": {
+            "firstName" : firstName, 
+            "lastName" : lastName,
+            "username" : username,
+            "emailAddress" : emailAddress
 
+        }})
+          
 
         }
       );
 
-console.log(user)
 
 
 
       const responseData = await response.json();
 console.log(responseData)
-setLastName(  responseData.user_metadata.family_name)
-      setLastName(responseData.message);
     } catch (error) {
-      setLastName(error.lastName);
     }
   };
 
@@ -70,7 +110,7 @@ setLastName(  responseData.user_metadata.family_name)
                 <h2>{name}</h2>
 <label>
            <p>First Name</p>
-           <input name="name" value={given_name}
+           <input name="name" value={firstName}
            onChange={e => setFirstName(e.target.value)}
 />
          </label>
@@ -84,20 +124,20 @@ setLastName(  responseData.user_metadata.family_name)
 
          <label>
            <p>Username</p>
-           <input name="name" value={nickname}
+           <input name="name" value={username}
            onChange={e => setUsername(e.target.value)}
 />
          </label>
 
          <label>
            <p>Email Address</p>
-           <input name="name" value={email}
+           <input name="name" value={emailAddress}
            onChange={e => setEmail(e.target.value)}
 />
          </label>
 
 
-         <button type="button" onClick={callSecureApi}>Submit</button>
+         <button type="button" onClick={updateUser}>Submit</button>
 
 
 
