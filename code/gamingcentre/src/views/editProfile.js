b2f8e9e5f71bb17c60606c5437209dec7e3a8e28
SimonFKings <sfk7@student.le.ac.uk>
2021-05-01 00:37:24 +0100

    Added CSS to edit profile page

--- code/gamingcentre/src/views/editProfile.js
+++ code/gamingcentre/src/views/editProfile.js
@@ -5,53 +5,56 @@ import { Loading } from "../components";
 // import { Button } from 'react-native';
 
 const EditProfile = () => {
-
-  
   const { user } = useAuth0();
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [username, setUsername] = useState("");
   const [emailAddress, setEmail] = useState("");
 
-
-  const {sub, given_name, family_name, nickname, email} = user;
-
+  const { sub, given_name, family_name, nickname, email, picture } = user;
 
   useEffect(() => {
     const getUserMetadata = async () => {
-
       try {
         const token = await getAccessTokenSilently();
 
         const response = await fetch(
-           `https://dev-22x3u4l0.us.auth0.com/api/v2/users/` + sub,
-          
-          {    
-  
+          `https://dev-22x3u4l0.us.auth0.com/api/v2/users/` + sub,
+
+          {
             headers: {
               Authorization: `Bearer ${token}`,
-  
-              
             },
           }
-          );
+        );
 
-          const responseData = await response.json();
-          setFirstName(responseData.user_metadata && responseData.user_metadata.firstName ? responseData.user_metadata.firstName : given_name)
-          setLastName(responseData.user_metadata && responseData.user_metadata.lastName ? responseData.user_metadata.lastName : family_name)
-          setUsername(responseData.user_metadata && responseData.user_metadata.username ? responseData.user_metadata.username  : nickname)
-          setEmail(responseData.user_metadata && responseData.user_metadata.emailAddress  ? responseData.user_metadata.emailAddress : email)
-         
-          
+        const responseData = await response.json();
+        setFirstName(
+          responseData.user_metadata && responseData.user_metadata.firstName
+            ? responseData.user_metadata.firstName
+            : given_name
+        );
+        setLastName(
+          responseData.user_metadata && responseData.user_metadata.lastName
+            ? responseData.user_metadata.lastName
+            : family_name
+        );
+        setUsername(
+          responseData.user_metadata && responseData.user_metadata.username
+            ? responseData.user_metadata.username
+            : nickname
+        );
+        setEmail(
+          responseData.user_metadata && responseData.user_metadata.emailAddress
+            ? responseData.user_metadata.emailAddress
+            : email
+        );
       } catch (e) {
         console.log(e.message);
-
       }
     };
     getUserMetadata();
   }, []);
-
-
 
   const { getAccessTokenSilently } = useAuth0();
 
@@ -59,91 +62,111 @@ const EditProfile = () => {
     try {
       const token = await getAccessTokenSilently();
 
-
       const response = await fetch(
-        `https://dev-22x3u4l0.us.auth0.com/api/v2/users/` + sub ,
-        {    
+        `https://dev-22x3u4l0.us.auth0.com/api/v2/users/` + sub,
+        {
           // mode: "no-cors",
-          method: 'PATCH',
+          method: "PATCH",
           headers: {
             Authorization: `Bearer ${token}`,
             // 'Access-Control-Allow-Origin' : '*',
             // 'Content-Type': 'application/json'
             // 'Accept': 'application/json',
-            'Content-Type': 'application/json'
+            "Content-Type": "application/json",
 
             // 'Access-Control-Allow-Methods' : '*',
             // "x-requested-with": 'XMLHttpRequest',
             // "origin" : "http://localhost:4040"
-
           },
-          body: JSON.stringify( { "user_metadata": {
-            "firstName" : firstName, 
-            "lastName" : lastName,
-            "username" : username,
-            "emailAddress" : emailAddress
-
-        }})
-          
-
+          body: JSON.stringify({
+            user_metadata: {
+              firstName: firstName,
+              lastName: lastName,
+              username: username,
+              emailAddress: emailAddress,
+            },
+          }),
         }
       );
 
-
-
-
       const responseData = await response.json();
-console.log(responseData)
-    } catch (error) {
-    }
+      console.log(responseData);
+    } catch (error) {}
   };
 
   return (
+    <div className="row">
+      <div class="col-md-4 border-right">
+        <div class="d-flex flex-column align-items-center text-center p-3 py-5">
+          <img
+            src={picture}
+            alt="Profile"
+            className="rounded-circle img-fluid profile-picture mb-3 mb-md-0"
+          />
+        </div>
+      </div>
 
-
-    <div>
-
-                <h2>{firstName} {lastName}</h2>
-<label>
-           <p>First Name</p>
-           <input name="name" value={firstName}
-           onChange={e => setFirstName(e.target.value)}
-/>
-         </label>
-
-         <label>
-           <p>Last Name</p>
-           <input name="name" defaultValue={lastName}
-            onChange={e => setLastName(e.target.value)}
-                   />
-         </label>
-
-         <label>
-           <p>Username</p>
-           <input name="name" value={username}
-           onChange={e => setUsername(e.target.value)}
-/>
-         </label>
-
-         <label>
-           <p>Email Address</p>
-           <input name="name" value={emailAddress}
-           onChange={e => setEmail(e.target.value)}
-/>
-         </label>
-
-
-         <button type="button" onClick={updateUser}>Submit</button>
-
-
-
-
-</div>
-
-
-)
+      <div class="col-md-8">
+        <div class="p-3 py-5">
+          <div class="row mt-2">
+            <div class="col-md-6">
+              <label>
+                <p>First Name</p>
+                <input
+                  name="name"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
+              </label>
+            </div>
+            <div class="col-md-6">
+              <label>
+                <p>Last Name</p>
+                <input
+                  name="name"
+                  defaultValue={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                />
+              </label>
+            </div>
+          </div>
+          <div class="row mt-3">
+            <div class="col-md-6">
+              <label>
+                <p>Username</p>
+                <input
+                  name="name"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </label>
+            </div>
+            <div class="col-md-6">
+              <label>
+                <p>Email Address</p>
+                <input
+                  name="name"
+                  value={emailAddress}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </label>
+            </div>
+          </div>
+          <div class="mt-5 text-right">
+            <button
+              className="btn btn-primary profile-button"
+              type="button"
+              onClick={updateUser}
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default withAuthenticationRequired(EditProfile, {
-    onRedirecting: () => <Loading />,
-  });
+  onRedirecting: () => <Loading />,
+});
